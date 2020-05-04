092cf46a4cd0096b2c1213118817b85b4384aa35
imykhailychenko <igor.c.m@ukr.net>
2020-05-04 17:05:29 +0300

    lang attribute

--- src/components/header/top-bar/locale/Locale.tsx
+++ src/components/header/top-bar/locale/Locale.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import styles from './Locale.module.css';
 
 interface Props {
@@ -7,14 +8,23 @@ interface Props {
 }
 
 const Locale: React.FC<Props> = ({ locale, toggleLocale }) => {
-  const ua = locale === 'UA' ? styles.active : styles.item ;
-  const ru = locale === 'RU' ? styles.active : styles.item ;
+  const ua = locale === 'UA' ? styles.active : styles.item;
+  const ru = locale === 'RU' ? styles.active : styles.item;
 
   return (
-    <ul className={styles.list}>
-      <li className={ua} onClick={() => toggleLocale('UA')}>UA</li>
-      <li className={ru} onClick={() => toggleLocale('RU')}>RU</li>
-    </ul>
+    <>
+      <Helmet>
+        <html lang={locale.toLowerCase()} />
+      </Helmet>
+      <ul className={styles.list}>
+        <li className={ua} onClick={() => toggleLocale('UA')}>
+          UA
+        </li>
+        <li className={ru} onClick={() => toggleLocale('RU')}>
+          RU
+        </li>
+      </ul>
+    </>
   );
 };
 
